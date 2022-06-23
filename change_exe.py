28cc6bb75c2bf7dc7873074ce5fa6be940bf9696
Syukri Faiz Nasir <syukrifaiz1995@gmail.com>
2022-06-23 15:44:45 +0800

    Update change_exe.py

--- change_exe.py
+++ change_exe.py
@@ -1,8 +1,8 @@
 import os, sys
-folder = 'C:/Users/Sharvin/Documents/txt_folder'
+folder = 'C:/Users/user1/Documents/batch_folder' #folder path that contains files to be change
 for filename in os.listdir(folder):
     infilename = os.path.join(folder,filename)
     if not os.path.isfile(infilename): continue
     oldbase = os.path.splitext(filename)
-    newname = infilename.replace('.csv', '.JSON')
+    newname = infilename.replace('.csv', '.JSON') #left = original ext, right = desired ext
     output = os.rename(infilename, newname)
